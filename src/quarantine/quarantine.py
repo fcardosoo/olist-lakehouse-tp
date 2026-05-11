77a25204328fa6e32366a546a2be2fc5d6d43093
fcardosoo <fabiano1.adm@gmail.com>
2026-05-11 01:11:39 +0000

    feat: implement quarantine validation layer

---
+++ src/quarantine/quarantine.py
@@ -0,0 +1,41 @@
+import dlt
+
+from pyspark.sql.functions import (
+    count,
+    col
+)
+
+
+@dlt.table(
+    name="quarantine_invalid_customers",
+    comment="Customers with fewer than 2 orders"
+)
+def quarantine_invalid_customers():
+
+    customers = spark.read.table("LIVE.silver_customers")
+
+    orders = spark.read.table("LIVE.silver_orders")
+
+    customer_order_counts = (
+        customers.alias("c")
+        .join(
+            orders.alias("o"),
+            "customer_id",
+            "left"
+        )
+        .groupBy(
+            "customer_id",
+            "customer_unique_id",
+            "customer_zip_code_prefix",
+            "customer_city",
+            "customer_state"
+        )
+        .agg(
+            count("order_id").alias("order_count")
+        )
+    )
+
+    return (
+        customer_order_counts
+        .filter(col("order_count") < 2)
+    )
