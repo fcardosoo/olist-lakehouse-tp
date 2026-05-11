72bdc45a72b2a69fcbfc99a73fa98f458f31e5a6
fcardosoo <fabiano1.adm@gmail.com>
2026-05-11 01:14:56 +0000

    feat: implement gold analytics layer

--- src/gold/gold.py
+++ src/gold/gold.py
@@ -1,4 +1,5 @@
-from pyspark import pipelines as dp
+import dlt
+
 from pyspark.sql.functions import (
     col,
     countDistinct,
@@ -7,31 +8,67 @@ from pyspark.sql.functions import (
     month,
     dayofmonth
 )
-@dp.table(
+
+
+@dlt.table(
     name="gold_customer_payment_analytics",
-    comment="Analytical table consolidating payments by customer, payment method, day and month"
+    comment="Analytical table for customer payments and purchases"
 )
 def gold_customer_payment_analytics():
 
-    customers = spark.read.table("ecommerce_lakehouse.silver.silver_customers")
-    orders = spark.read.table("ecommerce_lakehouse.silver.silver_orders")
-    payments = spark.read.table("ecommerce_lakehouse.silver.silver_payments")
+    customers = spark.read.table("LIVE.silver_customers")
+
+    orders = spark.read.table("LIVE.silver_orders")
+
+    payments = spark.read.table("LIVE.silver_payments")
+
     return (
+
         orders.alias("o")
-        .join(customers.alias("c"), "customer_id")
-        .join(payments.alias("p"), "order_id")
-        .groupBy(
-            "c.customer_unique_id",
-            "p.payment_type",
-            dayofmonth("o.order_purchase_timestamp").alias("purchase_day"),
-            month("o.order_purchase_timestamp").alias("purchase_month")
+
+        .join(
+            customers.alias("c"),
+            "customer_id"
         )
+
+        .join(
+            payments.alias("p"),
+            "order_id"
+        )
+
+        .groupBy(
+
+            col("c.customer_unique_id"),
+
+            col("p.payment_type"),
+
+            dayofmonth(
+                col("o.order_purchase_timestamp")
+            ).alias("purchase_day"),
+
+            month(
+                col("o.order_purchase_timestamp")
+            ).alias("purchase_month")
+        )
+
         .agg(
-            countDistinct("o.order_id").alias("order_count"),
-            sum("o.item_count").alias("total_items"),
-            sum("p.payment_value").alias("total_paid"),
-            avg("o.item_count").alias("avg_items"),
-            avg("p.payment_value").alias("avg_payment_value"),
-            avg("p.payment_installments").alias("avg_installments")
+
+            countDistinct("o.order_id")
+                .alias("order_count"),
+
+            sum("o.item_count")
+                .alias("total_items"),
+
+            sum("p.payment_value")
+                .alias("total_paid"),
+
+            avg("o.item_count")
+                .alias("avg_items"),
+
+            avg("p.payment_value")
+                .alias("avg_payment_value"),
+
+            avg("p.payment_installments")
+                .alias("avg_installments")
         )
     )
